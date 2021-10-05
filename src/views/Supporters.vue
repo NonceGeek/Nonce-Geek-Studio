596c15b9d937ba804fcedf5f306d5a4c3593ad39
Dream4ever <cnhwvista@gmail.com>
2021-10-05 22:55:37 +0800

    feat: 赞助机构页面用数据自动生成内容

--- src/views/Supporters.vue
+++ src/views/Supporters.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="supporters">
     <div class="header">支持机构</div>
-    <div class="supporter">
-      <span class="logo">
-        <img src="https://via.placeholder.com/100" alt="logo">
-      </span>
-      <span class="intro">上海对外经贸大学区块链 SIG</span>
-    </div>
-    <div class="supporter">
-      <span class="logo">
-        <img src="https://via.placeholder.com/100" alt="logo">
-      </span>
-      <span class="intro">南京大学金融x区块链 SIG</span>
-    </div>
-    <div class="supporter">
-      <span class="logo">
-        <img src="https://via.placeholder.com/100" alt="logo">
-      </span>
-      <span class="intro">微芒社区</span>
-    </div>
-    <div class="supporter">
-      <span class="logo">
-        <img src="https://via.placeholder.com/100" alt="logo">
-      </span>
-      <span class="intro">BeWater 开发者社区</span>
-    </div>
+    <a-card
+      v-for="supporter in supporters"
+      :key="supporter.name"
+      class="supporter"
+      hoverable
+    >
+      <img slot="cover" :src="supporter.logo" alt="">
+      <span class="intro">{{ supporter.name }}</span>
+    </a-card>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Supporters',
+  data() {
+    return {
+      supporters: [
+        {
+          name: '上海对外经贸大学区块链 SIG',
+          logo: 'https://via.placeholder.com/100',
+        },
+        {
+          name: '南京大学金融x区块链 SIG',
+          logo: 'https://via.placeholder.com/100',
+        },
+        {
+          name: '微芒社区',
+          logo: 'https://via.placeholder.com/100',
+        },
+        {
+          name: 'BeWater 开发者社区',
+          logo: 'https://via.placeholder.com/100',
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -48,17 +54,8 @@ export default {
   display: flex;
   align-items: center;
 
-  .logo {
-    width: 80px;
-    height: 80px;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
-  }
-
   .intro {
-    margin-left: 40px;
+    margin-left: 10px;
     font-size: 18px;
   }
 }
